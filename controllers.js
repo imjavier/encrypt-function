37f0540b602fcfe31c6a4d950e83de0a8c04a7ab
Javier Villarreal <villarrealjavier401@gmail.com>
2025-06-03 20:53:22 -0500

    feat: dynamo integration was done

--- controllers.js
+++ controllers.js
@@ -1,40 +1,45 @@
 import EncryptService from './services/encryptService.js';
 import  getErrorResponse  from './services/errorResponseService.js';
+import DynamoDBService from './services/dynamoService.js';
 
-export const createMessage = (event) => {
+export const createMessage = async (event) => {
     const { message, secretKey } = JSON.parse(event.body);
 
     if (!message || !secretKey)
         return getErrorResponse('Message and secret key are required', 400);
 
     const encryptedMessage = EncryptService.encrypt(message, secretKey);
-        //database logic
+    const isSaved = await DynamoDBService.createMessage(encryptedMessage, secretKey);
+
+    if (!isSaved)
+        return getErrorResponse('Error creating message', 500);
+
     return {
         statusCode: 200,
         body: JSON.stringify({ encryptedMessage }),
     };
 }
 
-export const myMessages = (event) => {
+export const myMessages = async (event) => {
     const secretKey = event.headers['X-Secret-Key'] || null;
 
     if (!secretKey)
         return getErrorResponse('Secret key is required', 400);
 
-    //database logic
-    const encryptedMessages = [];
-    const desencryptedMessages = encryptedMessages.map(encryptedMessage => 
+    const encryptedMessages = await DynamoDBService.getMyMessages(secretKey);
+    const desencryptedMessages = encryptedMessages.map(encryptedMessage =>
         EncryptService.decrypt(encryptedMessage, secretKey)
     );
+
     return {
         statusCode: 200,
         body: JSON.stringify({ messages: desencryptedMessages }),
     };
 }
 
-export const allMessages = (event) => {
-    // database logic
-    const encryptedMessages = [];
+export const allMessages = async (event) => {
+    const encryptedMessages = await DynamoDBService.getAllMessages();
+
     return {
         statusCode: 200,
         body: JSON.stringify({ messages: encryptedMessages }),
