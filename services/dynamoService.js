824690316378954c417b2b2210bea4e1b7b7da3d
Javier Villarreal <villarrealjavier401@gmail.com>
2025-06-03 20:51:20 -0500

    dynamo service was added

---
+++ services/dynamoService.js
@@ -0,0 +1,54 @@
+import { config, dynamoClient } from "../config/dynamoConfig.js";
+import { v4 } from "uuid";
+
+export class DynamoDBService {
+    static async createMessage(message, secretKey){
+        const params = {
+            TableName: config.tableName,
+            Item: {
+                id: v4(),
+                message: message,
+                secretKey: secretKey,
+            }
+        }
+        try {
+            await dynamoClient.put(params).promise();
+            return true;
+        } catch (error) {
+            console.error("Error creating message:", error);
+            return false
+        }
+    }
+    static async getMyMessages(secretKey) {
+        const params = {
+            TableName: config.tableName,
+            KeyConditionExpression: 'secretKey = :sk',
+            ExpressionAttributeValues: {
+                ':sk': secretKey
+            }
+        };
+
+        try{
+            const result = await dynamoClient.query(params).promise();
+            return result.Items || [];
+        }catch{
+            console.error("Error fetching messages:", error);
+            return [];
+        }
+    }
+
+    static async getAllMessages() {
+         const params = {
+            TableName: config.tableName
+        };
+
+        try {
+            const result = await dynamoClient.scan(params).promise();
+            return result.Items || [];
+        } catch (error) {
+            console.error("Error obteniendo todos los mensajes:", error);
+            return [];
+        }
+    }
+}
+
